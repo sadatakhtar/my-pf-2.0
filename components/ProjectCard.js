3a3369a96e53a5d5a1c0eca0831eb2143b1152f3
sadatakhtar <sadat0akhtar@gmail.com>
2021-08-05 23:57:28 +0100

    added shadow effect to project cards and border

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -17,7 +17,7 @@ function ProjectCard({Icon, title, techStack, link}) {
      </a>
 
       <p className='text-black pt-4 text-md'> {title}</p>
-      <p className='text-sm text-black pt-2 opacity-0 group-hover:opacity-100'><span className='text-[#092346]'>Technologies used:</span> {techStack}</p>
+      <p className='text-sm text-black pt-2 '><span className='text-[#092346]'>Technologies used:</span> {techStack}</p>
       <p className='text-sm text-black pt-2 cursor-pointer opacity-0 group-hover:opacity-100 hover:animate-bounce text-[#092346] hover:text-white
       '><a href={link}> Click here to view site</a></p>
       
