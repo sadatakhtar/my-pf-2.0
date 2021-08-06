907ee7fea4aabc3ebf7dfddf92dcab6382a4b082
sadat <sadat0akhtar@gmail.com>
2021-08-06 23:12:55 +0100

    sorted nav links, still need to add a tags

--- components/Nav.js
+++ components/Nav.js
@@ -1,21 +1,14 @@
 function Nav() {
-  const navHeadings = {
-    0: 'Home',
-    1: 'Projects',
-    2: 'Resume',
-    3: 'About',
-    4: 'Contact', 
-  }
+ 
 
   return (
-    <nav className='flex py-5 px-10 sm:px-20 text-sm md:text-lg whitespace-nowrap
-     space-x-10 sm:space-x-20 md:px-60 md:space-x-40 
-     overflow-x-scroll scrollbar-hide bg-[#092346] lg:justify-evenly border-opacity-10 '>
-      {Object.values(navHeadings).map((heading) => (
-        <h2 className='text-white cursor-pointer hover:text-blue-500 font-light'>{heading}</h2>
-      ))}
-      {/* add Ul & li tags instead */}
+    <nav className='flex justify-evenly list-none p-3 bg-[#092346]' >
+      <li className='text-white hover:text-gray-500'>HOME</li>
+      <li className='text-white hover:text-gray-500'>PROJECTS</li>
+      <li className='text-white hover:text-gray-500'>RESUME</li>
+      <li className='text-white hover:text-gray-500'>CONTACT</li>
     </nav>
+   
   )
 }
 
