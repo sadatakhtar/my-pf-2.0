158e14efaa4d873c2a5da6ce1b84423e41cf5965
sadat <sadat0akhtar@gmail.com>
2021-08-07 20:53:17 +0100

    tweaked next.config.js and package.json

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  // images: {
+  //   loader: (width, src,quality) => `/preprocessed-images/${filename}-${width}.jpg`
+  // },
+  images: {
+    loader: 'imgix',
+    path: '',
+  },
 }
